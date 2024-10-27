0c5f41af6afdd6ff689800d62024c9cbee6c2c86
ramyus889 <ramyus889.com>
2024-10-27 12:41:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup></script>
+
+<template>
+  <div class="mt-10">
+    <div className=" ">
+      <div className="mb-3   flex place-content-center px-5 ">
+        <div
+          data-aos="fade-up"
+          className="relative w-full rounded-[24px] border border-slate-700   sm:mx-auto sm:max-w-xl"
+        >
+          <div className="relative mx-8 rounded-3xl   py-5 shadow sm:p-10 sm:py-5 md:mx-0">
+            <div className=" mx-auto max-w-md">
+              <div className=" flex flex-col gap-3">
+                <div
+                  data-aos="fade-up"
+                  data-aos-delay="100"
+                  className=" z-10 text-center font-['Inter'] text-[20px] sm:text-[40px]"
+                >
+                  Авто Холод Каякент
+                </div>
+                <div className="w-full border-b border-slate-500 pb-5"></div>
+
+                <div
+                  className="flex w-full place-content-center items-center gap-5 rounded-xl border border-green-900 px-5 py-3"
+                >
+                  <div className="z-10 flex flex-col items-center">
+                    <div data-aos="fade-up" data-aos-delay="200" className="z-10">
+                      Написать в Whatsapp
+                    </div>
+                    <a
+                      data-aos="fade-up"
+                      data-aos-delay="250"
+                      href="https://wa.me/79689866661"
+                      target="_blank"
+                      className="z-[1000] font-bold underline"
+                    >
+                      7 968 986-66-61
+                    </a>
+                    <a
+                      href="https://wa.me/79625090505"
+                      target="_blank"
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                      className="z-[1000] font-bold underline"
+                    >
+                      7 962 509-05-05
+                    </a>
+                  </div>
+                </div>
+
+                <div className="w-full border-b border-slate-500 "></div>
+                <a
+                  href="https://maps.app.goo.gl/qqcRE8Xi4DM1NtDr7"
+                  target="_blank"
+                  data-aos="fade-up"
+                  data-aos-delay="300"
+                  className="z-10 text-green-700 hover:underline"
+                >
+                  улица Кавказ, 80-82, Каякентский район, Дагестан, Россия, 368301
+                </a>
+                <div className="z-10 w-full">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1508.737273182357!2d47.950025190655296!3d42.37494085384711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sru!2sru!4v1730021689153!5m2!1sru!2sru"
+                    width="100%"
+                    class="rounded-3xl xl:h-[450px] h-[300px]"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5 border-t border-slate-500">
+            <div className="py-3 ps-5">Auto Holod Kayakent © 2024</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/router/routes.js
@@ -0,0 +1,58 @@
+// import UseObjectDifferent from '@/AllComponents/Different/UseObjectDifferent.vue';
+// import UseTailwindDifferent from '@/AllComponents/Different/UseTailwindDifferent.vue';
+// import PrimeComponents from '@/AllComponents/PrimeComponents/PrimeComponents.vue';
+// import UseMainRandom from '@/AllComponents/RandomNumber/UseMainRandom.vue';
+// import UseTextG from '@/AllComponents/TextGenerate/UseTextG.vue';
+// import UseDataBase from '@/AllComponents/VideoCourse/components/UseDataBase.vue';
+// import UseDirective from '@/AllComponents/VideoCourse/components/UseDirective.vue';
+// import UseVueMotion from '@/AllComponents/VueMotion/UseVueMotion.vue';
+// import RouterVue from '@/AllComponents/VueRouter/routerVue.vue';
+// import UseSecret from '@/AllComponents/VueSecret/UseSecret.vue';
+// import AppCourse from '@/AppCourse.vue';
+
+// export const routes = [
+//   {
+//     path: '/App-Course',
+//     component: AppCourse
+//   },
+//   {
+//     path: '/vue-router',
+//     component: RouterVue
+//   },
+//   {
+//     path: '/prime-components',
+//     component: PrimeComponents
+//   },
+//   {
+//     path: '/object-different',
+//     component: UseObjectDifferent
+//   },
+//   {
+//     path: '/usedatabase',
+//     component: UseDataBase
+//   },
+//   {
+//     path: '/usedirective',
+//     component: UseDirective
+//   },
+//   {
+//     path: '/tailwind',
+//     component: UseTailwindDifferent
+//   },
+//   {
+//     path: '/usemainrandom',
+//     component: UseMainRandom
+//   },
+//   {
+//     path: '/secretkey',
+//     component: UseSecret
+//   },
+//   {
+//     path: '/usetextG',
+//     component: UseTextG
+//   },
+//   {
+//     path: '/UseVueMotion',
+//     component: UseVueMotion
+//   }
+// ];
